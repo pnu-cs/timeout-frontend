06ef539bd6bc48458a619e0f2f317f1b386464f3
Nadiia <nfome@softserveinc.com>
2021-11-21 23:35:50 +0200

    display products

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import SwiperComponent from "../../components/Swiper";
-
+import {ClockLoader} from 'react-spinners';
 import {selectIsProductsLoading, selectProducts} from "../../redux/product/selectors";
 
 
@@ -10,7 +10,11 @@ export const HomePage = () => {
     const isLoading = useSelector(selectIsProductsLoading);
 
     if (isLoading) {
-        return <div>Loading...</div>
+        return <div style={{height: '80vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><ClockLoader color={'gray'}/></div>
+    }
+
+    if(!products || products.length === 0) {
+        return <div style={{height: '80vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'darkgray'}}>Error loading products...</div>
     }
 
     return  <SwiperComponent items={products} />
--- src/redux/product/sagas/fetchProductsSaga.ts
+++ src/redux/product/sagas/fetchProductsSaga.ts
@@ -1,43 +1,68 @@
 import {put} from "redux-saga/effects";
-import {fetchProductsSucceed} from "../actions";
+import {fetchProductsFailed, fetchProductsSucceed} from "../actions";
+import {push} from "react-router-redux";
+
+const GET_PRODUCTS_PATH = 'http://localhost:8080/products';
 
 export default function* fetchProductsSags() {
-    yield setTimeout(() => {}, 7000);
+    let response: object;
 
-    yield put(fetchProductsSucceed([
-        {
-            title: 'Product 1',
-            price: '250$',
-            description: 'Classic Bayswater is an effortless day-yo-evening timepiece.',
-            useFor: 'For important meetings.',
-            brand: 'Brand: Rolex',
-            model: 'Daytona',
-            yearOfIssue:  1968,
-            housingMaterial: 'Housing material: steel',
-            strapMaterial:'Strap material: crocodile skin.',
-            dialColor: 'Dial color: black',
-            strapColor: 'Strap color: white',
-            housingThickness: 'Housing thickness: 37 mm',
-            strapLength: 'Strap length: 150mm',
-            clockWork: 'The watch works: mechanically / from the battery',
-            waterResistance: 'Water resistance of the watch: up to 3 atmospheres',
-        },
-        {
-            title: 'Product 2',
-            price: '250$',
-            description: 'Classic Bayswater is an effortless day-yo-evening timepiece.',
-            useFor: 'For important meetings.',
-            brand: 'Brand: Rolex',
-            model: 'Daytona',
-            yearOfIssue:  1968,
-            housingMaterial: 'Housing material: steel',
-            strapMaterial:'Strap material: crocodile skin.',
-            dialColor: 'Dial color: black',
-            strapColor: 'Strap color: white',
-            housingThickness: 'Housing thickness: 37 mm',
-            strapLength: 'Strap length: 150mm',
-            clockWork: 'The watch works: mechanically / from the battery',
-            waterResistance: 'Water resistance of the watch: up to 3 atmospheres',
-        }
-    ]));
+    try {
+        yield fetch(GET_PRODUCTS_PATH, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(res => res.json())
+            .then(data => response = data)
+            .catch(e => {
+                throw new Error('FETCH PRODUCTS ERROR');
+            });
+
+        // @ts-ignore
+        yield put(fetchProductsSucceed(response));
+        yield put(push('/'));
+    } catch (error: any) {
+        console.error('FETCH PRODUCTS ERROR', error?.message);
+        yield put(fetchProductsFailed(error));
+    }
+
+    // yield put(fetchProductsSucceed([
+    //     {
+    //         title: 'Product 1',
+    //         price: '250$',
+    //         description: 'Classic Bayswater is an effortless day-yo-evening timepiece.',
+    //         useFor: 'For important meetings.',
+    //         brand: 'Brand: Rolex',
+    //         model: 'Daytona',
+    //         yearOfIssue:  1968,
+    //         housingMaterial: 'Housing material: steel',
+    //         strapMaterial:'Strap material: crocodile skin.',
+    //         dialColor: 'Dial color: black',
+    //         strapColor: 'Strap color: white',
+    //         housingThickness: 'Housing thickness: 37 mm',
+    //         strapLength: 'Strap length: 150mm',
+    //         clockWork: 'The watch works: mechanically / from the battery',
+    //         waterResistance: 'Water resistance of the watch: up to 3 atmospheres',
+    //     },
+    //     {
+    //         title: 'Product 2',
+    //         price: '250$',
+    //         description: 'Classic Bayswater is an effortless day-yo-evening timepiece.',
+    //         useFor: 'For important meetings.',
+    //         brand: 'Brand: Rolex',
+    //         model: 'Daytona',
+    //         yearOfIssue:  1968,
+    //         housingMaterial: 'Housing material: steel',
+    //         strapMaterial:'Strap material: crocodile skin.',
+    //         dialColor: 'Dial color: black',
+    //         strapColor: 'Strap color: white',
+    //         housingThickness: 'Housing thickness: 37 mm',
+    //         strapLength: 'Strap length: 150mm',
+    //         clockWork: 'The watch works: mechanically / from the battery',
+    //         waterResistance: 'Water resistance of the watch: up to 3 atmospheres',
+    //     }
+    // ]));
 }
